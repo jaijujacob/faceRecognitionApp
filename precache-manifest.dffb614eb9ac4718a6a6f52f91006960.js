bafba7b1941e8eecdfb6d6991bea1bf9b49b9210
Jaiju Jacob <jaijujacob@gmail.com>
2020-04-22 15:35:20 +0530

    Updates

---
+++ precache-manifest.dffb614eb9ac4718a6a6f52f91006960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16931954ad0f4b5b7436b8c63f431f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18d62ee68293720728f",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "15127545b6647f90e031",
+    "url": "/static/css/main.3aab45da.chunk.css"
+  },
+  {
+    "revision": "f18d62ee68293720728f",
+    "url": "/static/js/2.ded9f834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ded9f834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15127545b6647f90e031",
+    "url": "/static/js/main.af110632.chunk.js"
+  },
+  {
+    "revision": "886d0df223d3918924dd",
+    "url": "/static/js/runtime-main.33111a8f.js"
+  }
+]);
