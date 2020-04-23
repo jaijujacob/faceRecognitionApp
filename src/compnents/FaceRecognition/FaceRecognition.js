1f634d625b7b7a338e227021531fb74bc6a46ba6
Jaiju Jacob <jaijujacob@gmail.com>
2020-04-23 09:47:42 +0530

    Adding signin and client evet handling functionalities

--- src/compnents/FaceRecognition/FaceRecognition.js
+++ src/compnents/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center'>
             <div className="absolute mt2">
-                <img id='inputimage' src={imageUrl} alt="Recognized image" width='500px' height='auto' />
+                <img id='inputimage' src={imageUrl} alt="Recognized face" width='500px' height='auto' />
                 <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             </div>
         </div>
